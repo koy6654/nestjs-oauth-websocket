b394698cefb6505221d268d1da659003ddf59e26
koy6654 <koy410994@gmail.com>
2023-11-15 13:45:41 +0900

    프로젝트 구조 도메인형으로 변경

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { ModuleController } from './service/module/module.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, ModuleController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
